66aa85f76bb789e29ef139cbf88643c91dc95705
JessicaPe7 <je-perez@outlook.com>
2024-12-17 11:48:50 -0300

    arreglos

--- JavaScript/productos.js
+++ JavaScript/productos.js
@@ -78,20 +78,3 @@ document.addEventListener('click', (event) => {
 });
 
 
-
-
-
-
-//CANTIDAD DE PRODUCTOS EN CARRITO
-let contador = 0;
-//console.log("largo de carrito objetos", carrito.length);
-for (let i = 0; i < carrito.length; i++) {
-    let cuanto = carrito[i].cantidad.valueOf();
-    //console.log("el valor contenido es: ", cuanto);
-    contador = contador + cuanto;
-}
-//console.log("la cantidad de pordictos en carrito es: ",contador);
-
-const artcantidad = document.getElementById('cantidadart').innerHTML += contador;
-
-
