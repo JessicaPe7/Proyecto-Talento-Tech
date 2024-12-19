09e8307fe758b393903e7592ee18aa5f2c88f031
JessicaPe7 <je-perez@outlook.com>
2024-12-19 16:15:35 -0300

    aviso de carrito vacio

--- JavaScript/cantidadcarrito.js
+++ JavaScript/cantidadcarrito.js
@@ -15,13 +15,9 @@
 
 carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-const cantca = document.querySelector('.cantidad-carrito');
-
-
 let contador = 0;
 //console.log("largo de carrito objetos", carrito.length);
 for (let i = 0; i < carrito.length; i++) {
-    
 
     let cuanto = carrito[i].cantidad.valueOf();
     //console.log("el valor contenido es: ", cuanto);
@@ -31,11 +27,4 @@ for (let i = 0; i < carrito.length; i++) {
 
 //console.log("la cantidad de pordictos en carrito es: ",contador);
 
-const html = `
-    <p class="cantidad-carrito" id="cantidadart">${contador}</p>
-`
-console.log("contador es: ", contador);
-console.log("contenido del html:", html);
-cantca.innerHTML += html;
-
-//const artcantidad = document.getElementById('cantidadart').innerHTML += contador;
+const artcantidad = document.getElementById('cantidadart').innerHTML += contador;
--- JavaScript/carrito.js
+++ JavaScript/carrito.js
@@ -1,6 +1,16 @@
 carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-console.log(carrito);
+console.log("tiene:", carrito);
+
+if(carrito.length == 0){
+    const aviso = document.querySelector(".p");
+    const p = `
+        <h2 class="texto2">El carrito esta vacio</h2>
+    `;
+    console.log(aviso);
+    console.log(p);
+    aviso.innerHTML += p;
+}
 
 const items = document.querySelector(".items");
 
@@ -10,9 +20,7 @@ carrito.forEach((item) => {
     const html = `
         <ul data-id="${item.id}" class="items-carrito">
             <li class="info-carrito">${item.nombre}</li>
-            
             <li class="item-especial"><input type="image" src="/img/plus.png" class="ico2">${item.cantidad}<input type="image" src="/img/minus.png" class="ico2"></li>
-            
             <li>${item.precio}</li>
             <li>${item.cantidad * item.precio}</li>
             <input class="elim" type="image" src="img/trash.png" alt="eliminar">
