dae78944ddad3d8a6144f4311f86d57938e2f89f
JessicaPe7 <je-perez@outlook.com>
2024-12-20 12:44:41 -0300

    arreglo de vista para celular del carrito

--- JavaScript/carrito.js
+++ JavaScript/carrito.js
@@ -18,32 +18,55 @@ items.innerHTML = "";
 
 carrito.forEach((item) => {
     const html = `
-        <ul data-id="${item.id}" class="items-carrito">
-            <li class="info-carrito">${item.nombre}</li>
-            <li class="item-especial"><input type="image" src="/img/plus.png" class="ico2">${item.cantidad}<input type="image" src="/img/minus.png" class="ico2"></li>
-            <li>${item.precio}</li>
-            <li>${item.cantidad * item.precio}</li>
-            <input class="elim" type="image" src="img/trash.png" alt="eliminar">
-        </ul>
+        <article data-id="${item.id}" class="items-carrito">
+            
+            <div class="titulo-borrar">
+                <h3 class="carrito-nombre">${item.nombre}</h3>
+                <input class="elim ico2" type="image" src="img/trash.png" alt="eliminar">
+            </div>
+
+            <div class="item-especial">
+                <input type="image" src="/img/plus.png" class="ico2">${item.cantidad}<input type="image" src="/img/minus.png" class="ico2">
+            </div>
+
+            <div class="info-precio">
+                <p>$${item.precio}</p>
+                <p>$${item.cantidad * item.precio}</p> 
+            </div>
+            
+            
+        </article>
+        
     `;
     items.innerHTML += html;
     
 });
 
+//calcula el total
+let totalcompra = 0;
+
+for (let i = 0; i < carrito.length; i++) {
+
+    let cantidad = carrito[i].cantidad.valueOf();
+    let precio = carrito[i].precio.valueOf();
+    totalcompra = totalcompra + (cantidad * precio);
+
+}
+const final = document.getElementById('total-final').innerHTML += "$";
+const final2 = document.getElementById('total-final').innerHTML += totalcompra
 
 
 
+//elimina el producto
 document.addEventListener('click', (event) => {
     
-    //console.log(event.target);
+    console.log(event.target);
 
     if (event.target.classList.contains("elim")){
-        //console.log("entro2");
-        const id = event.target.closest('ul').dataset.id;
-        //console.log(id);
+
+        const id = event.target.closest('article').dataset.id;
 
         const elemento2 = carrito.findIndex((item) => item.id == id);
-        console.log("el indice del elemento es: ", elemento2);
             
         if (elemento2 !== -1) {
             carrito.splice(elemento2, 1);            
